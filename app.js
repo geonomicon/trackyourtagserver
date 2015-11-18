1f6a9304cc718125ca13c559af1d577dcf72cf2c
bhuvan malik <bhuvanmalik@bhuvans-MacBook-Pro.local>
2015-11-18 17:39:16 +0530

    b

--- app.js
+++ app.js
@@ -32,6 +32,22 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+
+/*--------------------------------------------*/
+app.get('/',function(req,res){
+res.send("Welcome to Track Your Tag Node.js Server");
+});
+
+
+app.get('/:id/:password',function(req,res){
+  if(req.params.id=="ad@min" && req.params.password=="admin")
+  res.send("valid");
+  else
+  res.send("invalid");
+});
+
+/*--------------------------------------------*/
+
 // error handlers
 
 // development error handler
